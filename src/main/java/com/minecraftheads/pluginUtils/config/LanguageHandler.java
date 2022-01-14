3a3c1e80d810db67e98fb5565bfac2cde459dade
LordRazen <razen.mailer@gmail.com>
2022-01-14 17:55:34 +0100

    Language Fix

--- src/main/java/com/minecraftheads/pluginUtils/config/LanguageHandler.java
+++ src/main/java/com/minecraftheads/pluginUtils/config/LanguageHandler.java
@@ -29,6 +29,6 @@ public class LanguageHandler {
                 new File(plugin.getDataFolder(), "languages/" + lang + ".yml"));
 
         // Return the correct string
-        return " §r" + langFile.getString(section);
+        return langFile.getString(section);
     }
 }
